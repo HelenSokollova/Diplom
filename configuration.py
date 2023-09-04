39cc600e474aedef139e46fe996c4b1e11568645
HelenSokollova <qwezxc.sokolova@yandex.ru>
2023-09-04 15:18:08 +0100

    Изменения в коде

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://afe290fa-7bfa-4a10-ae9b-a7f87c33c491.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+TAKE_TRACK_PATH = "/api/v1/orders/track?t="
---
+++ test_order_info_by_track.py
@@ -0,0 +1,9 @@
+# Елена Соколова, 8-я когорта — Финальный проект. Инженер по тестированию плюс
+
+import sender_stand_request
+import data
+
+def test_code_is_200():
+    track = sender_stand_request.create_order(data.order_body).json()['track']
+    response = sender_stand_request.get_order_info_by_track(track)
+    assert response.status_code == 200
